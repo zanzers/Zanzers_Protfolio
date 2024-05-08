2700eed98e63c8a20b373752210694f2bbe3ef91
zanzers <nowellsaavedra999@gmail.com>
2024-05-08 19:44:57 +0800

    1st upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Route , BrowserRouter as Router, Routes } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import { Home, About, Projects, Contacts} from "./pages"
+
+const App = () => {
+  return (
+    <main className="bg-slate-300/20">
+       
+        <Router>
+           < Navbar/>
+            <Routes>
+                <Route path="/" element={<Home/>} />
+                <Route path="/about" element={<About/>} />
+                <Route path="/projects" element={<Projects/>} />
+                <Route path="/contact" element={<Contacts/>} />
+            </Routes>
+        </Router>
+    </main>
+  )
+}
+
+export default App
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Home from './Home'
+import About from './About'
+import Projects from './Projects'
+import Contacts from './Contact'
+
+
+
+
+export { Home, About, Projects, Contacts};
